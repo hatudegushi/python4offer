e852c9764a8f2e2d1573a3cb2dfdced0209c64ea
hatu <lazy_hatu@foxmail.com>
2020-03-15 11:40:58 +0800

    update

--- 20包含min函数的栈/main.py
+++ 20包含min函数的栈/main.py
@@ -1,14 +1,34 @@
 # -*- coding:utf-8 -*-
 class Solution:
+    def __init__(self):
+        self.stack = []
+        self.mstack = []
     def push(self, node):
         # write code here
-        pass
+        self.stack.append(node)
+        if not self.mstack:
+            self.mstack.append(node)
+        else:
+            if self.mstack[-1] < node:
+                self.mstack.append(self.mstack[-1])
+            else:
+                self.mstack.append(node)
     def pop(self):
         # write code here
-        pass
+        if self.stack:
+            self.stack.pop()
+            self.mstack.pop()
+
     def top(self):
         # write code here
-        pass
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return []
+
     def min(self):
         # write code here
-        pass
+        if self.mstack:
+            return self.mstack[-1]
+        else:
+            return []
---
+++ 21栈的压入、弹出序列/main.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def IsPopOrder(self, pushV, popV):
+        # write code here
+        pass
