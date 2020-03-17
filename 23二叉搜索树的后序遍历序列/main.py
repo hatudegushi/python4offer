8866e2bb6c0249c359f285a29b27c914c89d6c2a
hatu <lazy_hatu@foxmail.com>
2020-03-17 12:05:08 +0800

    update

--- 23二叉搜索树的后序遍历序列/main.py
+++ 23二叉搜索树的后序遍历序列/main.py
@@ -1,5 +1,23 @@
 # -*- coding:utf-8 -*-
 class Solution:
+    def assistance(self, sequence):
+        if not sequence:
+            return True
+        position = 0
+        while sequence[position] < sequence[-1]:
+            position += 1
+        i = position + 1
+        length = len(sequence)
+        while i < length:
+            if sequence[i] < sequence[-1]:
+                return False
+            i += 1
+        left = self.assistance(sequence[0:position])
+        right = self.assistance(sequence[position:-1])
+        return left and right
+
     def VerifySquenceOfBST(self, sequence):
         # write code here
-        pass
+        if not sequence:
+            return False
+        return self.assistance(sequence)
